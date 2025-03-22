8e907a25043492edbd0bf6f9215b6589992357fe
petermunene <peter.munene@student.moringaschool.com>
2025-03-22 17:42:47 +0300

    communicating with the server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+
+
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{"content-type":"application/json",
+                "accept":"application/json"
+       },
+        body:JSON.stringify({name,email})
+})
+       .then(response=>response.json())
+       .then(data=>{console.log(data)
+        document.body.innerHTML = data.id;
+       })
+       .catch(function(error){
+        alert("error getting data")
+        document.body.innerHTML=error.message
+       })
+    
+}
